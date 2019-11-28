5b48c48e0ea3d75555963065f0533ba1ec9a9ee1
Martín Villanueva <martin.villanueva@usm.cl>
2019-11-27 21:24:32 -0300

    Adds script to train a single cross-validated model

---
+++ scripts/single_model_cv.py
@@ -0,0 +1,113 @@
+import sys
+import time
+import argparse
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from tsforest import forecaster
+from utils import reduce_mem_usage
+from config import get_model_params
+from sklearn.model_selection import StratifiedKFold
+
+# timestamp of the starting execution time
+timestamp = datetime.now().strftime("%Y/%m/%d, %H:%M:%S").replace("/","-").replace(" ","")
+
+AVAILABLE_CLASSES = ["CatBoostForecaster",
+                     "LightGBMForecaster",
+                     "XGBoostForecaster",
+                     "H2OGBMForecaster"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m",
+                    "--model_class", 
+                    type=str)
+args = parser.parse_args()
+
+model_class_name = args.model_class
+if model_class_name not in AVAILABLE_CLASSES:
+    print(f"{model_class_name} is not a valid model class.")
+    sys.exit()
+model_class = getattr(forecaster, model_class_name)
+
+# file logger
+logger = open(f"results/{model_class_name}_smcv_{timestamp}.meta", "w")
+
+print("[INFO] loading data")
+tic = time.time()
+train_data = pd.read_hdf('data/train_data.h5', 'train_data')
+train_data.rename({"timestamp":"ds", "meter_reading":"y"}, axis=1, inplace=True)
+test_data = pd.read_hdf('data/test_data.h5', 'test_data')
+test_data.rename({"timestamp":"ds"}, axis=1, inplace=True)
+tac = time.time()
+print(f"[INFO] time elapsed loading data: {(tac-tic)/60.} min.\n")
+
+print("[INFO] generating validation data")
+tic = time.time()
+splitter = StratifiedKFold(n_splits=4, shuffle=False, random_state=23)
+valid_indexes = [valid_index for _,valid_index in splitter.split(train_data, train_data['building_id'])]
+tac = time.time()
+print(f"[INFO] time elapsed generating validation data: {(tac-tic)/60.} min.\n")
+
+model_kwargs = {"model_params":get_model_params(model_class_name),
+                "feature_sets":['calendar', 'calendar_cyclical'],
+                "exclude_features":["year","days_in_month"],
+                "categorical_features":{"building_id":"default",
+                                        "meter":"OneHotEncoder",
+                                        "site_id":"default",
+                                        "primary_use":"default"},
+                "ts_uid_columns":["building_id","meter"],
+                "detrend":False,
+                "target_scaler":None}
+
+all_predictions = list()
+for i,valid_index in enumerate(valid_indexes):
+    fcaster = model_class(**model_kwargs)
+
+    print(f"[INFO] preparing the features - fold: {i}")
+    tic = time.time()
+    fcaster.prepare_features(train_data=train_data, valid_index=valid_index)
+    fcaster.train_features = reduce_mem_usage(fcaster.train_features)
+    fcaster.valid_features = reduce_mem_usage(fcaster.valid_features)
+    tac = time.time()
+    print(f"[INFO] time elapsed preparing the features: {(tac-tic)/60.} min.\n")
+
+    print(f"[INFO] fitting the model - fold: {i}")
+    tic = time.time()
+    fcaster.fit()
+    tac = time.time()
+    print(f"[INFO] time elapsed fitting the model: {(tac-tic)/60.} min.\n")
+
+    print(f"[INFO] evaluating the model - fold: {i}")
+    tic = time.time()
+    valid_error = fcaster.evaluate(train_data.loc[valid_index, :], metric="rmsle")
+    logger.write(f"error on fold {i}: {valid_error}\n")
+    logger.write(f"best_iteration on fold {i}: {fcaster.best_iteration}\n")
+    tac = time.time()
+    print(f"[INFO] time elapsed evaluating the model: {(tac-tic)/60.} min.\n")
+
+    print(f"[INFO] predicting - fold: {i}")
+    tic = time.time()
+    predictions = fcaster.predict(test_data)
+    idx = predictions.query("y_pred < 0").index
+    predictions.loc[idx, "y_pred"] = 0
+    tac = time.time()
+    print(f"[INFO] time elapsed predicting: {(tac-tic)/60.} min.\n")
+    
+    fcaster.save_model(f"results/{model_class}_fold{i}_{timestamp}.model")
+    all_predictions.append(predictions.y_pred.values)
+
+logger.write(f"model_params: {fcaster.model_params}\n")
+logger.write(f"input_features: {fcaster.input_features}\n")
+
+print("[INFO] saving submission")
+tic = time.time()
+predictions = np.mean(np.asarray(all_predictions), axis=0)
+submission = pd.DataFrame({"row_id":test_data.row_id.values,
+                           "meter_reading":predictions})
+submission.to_csv(f"results/{model_class_name}_smcv_{timestamp}.csv.gz", 
+                  index=False, 
+                  compression="gzip")
+tac = time.time()
+print(f"[INFO] time elapsed saving submission: {(tac-tic)/60.} min.\n")
+
+logger.close()
